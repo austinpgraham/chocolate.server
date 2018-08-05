d1122a673cd81fdd6076149b9d6fbfca30af485b
Austin Graham <austingraham731@gmail.com>
2018-08-05 16:50:51 -0500

    logout and auth required

--- pkg/user/security.go
+++ pkg/user/security.go
@@ -1,17 +1,18 @@
-
 package user
 
 import (
-	   "github.com/jinzhu/gorm"
+	"errors"
+
 	db "github.com/austinpgraham/chocolate.server/pkg/database"
+	"github.com/jinzhu/gorm"
 )
 
 const SESSION_TABLE = "sessions"
 
 type Session struct {
 	AssocUser User `gorm:"foreignkey:UserID;association_foreignkey:UserID"`
-	UserID uint `gorm:"unique"`
-	Cookie string
+	UserID    uint `gorm:"unique"`
+	Cookie    string
 }
 
 func checkSessionTable(db *gorm.DB) {
@@ -27,9 +28,33 @@ func SaveSession(uid uint, cookie string) error {
 	session := Session{UserID: uid, Cookie: cookie}
 	err := db.Create(&session).Error
 	if err != nil {
-		db.Where("user_id = ?", uid).Delete(Session{})
+		RemoveSession(uid)
 		err = db.Create(&session).Error
 		return err
 	}
 	return nil
+}
+
+func RemoveSession(uid uint) {
+	db, _ := db.GetConnection()
+	defer db.Close()
+	checkSessionTable(db)
+	db.Where("user_id = ?", uid).Delete(Session{})
+}
+
+func GetSession(uid uint) *Session {
+	db, _ := db.GetConnection()
+	defer db.Close()
+	checkSessionTable(db)
+	session := Session{}
+	db.Where("user_id = ?", uid).First(&session)
+	return &session
+}
+
+func CheckSession(uid uint, cookie string) error {
+	session := GetSession(uid)
+	if session.Cookie == cookie {
+		return nil
+	}
+	return errors.New("Session invalid.")
 }
