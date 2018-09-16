1a9d24435f94263ec3f2eed2e5d4164348768677
Austin Graham <austingraham731@gmail.com>
2018-09-16 11:42:46 -0500

    route to get group

--- internal/app/neighborhood/group.go
+++ internal/app/neighborhood/group.go
@@ -2,11 +2,14 @@ package neighborhood
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	er "github.com/austinpgraham/chocolate.server/internal/app/error"
 	"github.com/austinpgraham/chocolate.server/internal/app/user"
 	"github.com/austinpgraham/chocolate.server/pkg/neighborhood"
+	muser "github.com/austinpgraham/chocolate.server/pkg/user"
+	"github.com/gorilla/mux"
 )
 
 func CreateNeighborhood(w http.ResponseWriter, r *http.Request) {
@@ -29,4 +32,26 @@ func CreateNeighborhood(w http.ResponseWriter, r *http.Request) {
 	nb.Admin = *containedUser
 	neighborhood.CreateNeighborhood(&nb)
 	w.WriteHeader(http.StatusCreated)
+}
+
+func GetNeighborhood(w http.ResponseWriter, r *http.Request) {
+	authUser := user.ReqAuth(w, r)
+	if authUser == nil {
+		err := er.Error{Code: "LoginRequired.", Message: "Login Required."}
+		w.WriteHeader(http.StatusForbidden)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+	nb := mux.Vars(r)["neighborhood"]
+	neigh := neighborhood.GetNeighborhood(neighborhood.NAME, nb)
+	if neigh == nil {
+		err := er.Error{Code: "NeighborhoodNotFound", Message: "Neighborhood not found."}
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+	neigh.Password = ""
+	neigh.Admin = *muser.GetUser("ID", fmt.Sprintf("%d", authUser.Model.ID))
+	neigh.Admin.Password = ""
+	json.NewEncoder(w).Encode(neigh)
 }
--- internal/app/router/router.go
+++ internal/app/router/router.go
@@ -15,4 +15,5 @@ func DefineRoutes(router *mux.Router) {
 	router.HandleFunc("/auth/logout", user.Logout).Methods("GET")
 	router.HandleFunc("/chocolate/courses", course.CreateCourse).Methods("POST")
 	router.HandleFunc("/doorbell/groups", neighborhood.CreateNeighborhood).Methods("POST")
+	router.HandleFunc("/doorbell/groups/{neighborhood}", neighborhood.GetNeighborhood).Methods("GET")
 }
