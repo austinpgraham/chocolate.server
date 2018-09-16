94102fd6e93292316c0cd6b30ed29994db1068dc
Austin Graham <austingraham731@gmail.com>
2018-09-16 11:16:59 -0500

    generate join token string

--- pkg/neighborhood/group.go
+++ pkg/neighborhood/group.go
@@ -2,6 +2,7 @@ package neighborhood
 
 import (
 	"fmt"
+	"math/rand"
 
 	"github.com/jinzhu/gorm"
 
@@ -9,13 +10,18 @@ import (
 	"github.com/austinpgraham/chocolate.server/pkg/user"
 )
 
+const TOKEN_LENGTH = 16
 const NEIGHBORHOODS_TABLE = "neighborhoods"
+
+var LETTER_RUNES = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 type Neighborhood struct {
 	gorm.Model
-	Name    string    `json:"name" gorm:"unique_index"`
-	Admin   user.User `json:"admin" gorm:"foreignkey:AdminID"`
-	AdminID uint      `json:"admin_id"`
+	Name      string    `json:"name" gorm:"unique_index"`
+	Admin     user.User `json:"admin" gorm:"foreignkey:AdminID"`
+	AdminID   uint      `json:"admin_id"`
+	Password  string    `json:"password,omitempty"`
+	JoinToken string    `json:"join_token"`
 }
 
 func checkTable() {
@@ -26,10 +32,20 @@ func checkTable() {
 	}
 }
 
+func generateToken(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = LETTER_RUNES[rand.Intn(len(LETTER_RUNES))]
+	},
+	return string(b)
+}
+
 func CreateNeighborhood(neigh *Neighborhood) error {
 	checkTable()
 	db, _ := db.GetConnection()
 	defer db.Close()
+	token := generateToken(TOKEN_LENGTH)
+	neigh.JoinToken = token
 	err := db.Create(neigh).Error
 	return err
 }
